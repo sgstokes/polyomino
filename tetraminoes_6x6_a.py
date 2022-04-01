3ebf2e1f862efdfd6e2ecbe0186d2532f3f96ca8
sgstokes <s@sgstokes>
2022-04-01 12:36:33 -0400

    202204011236

--- tetraminoes_6x6_a.py
+++ tetraminoes_6x6_a.py
@@ -1,9 +1,12 @@
 import poly.parser as parser
 import poly.solver as solver
 
-figure = parser.load("shapes/figure.pol")
-part = parser.load("shapes/_part.pol")
+figure = parser.load("shapes/figure_6x6_a.pol")
+part = parser.load("shapes/X5.pol")
 
 for idx, s in enumerate(solver.split_into_parts(figure, part)):
     parser.save_solution_to_svg(s, "output/" + str(idx) + "th_sol.svg")
     parser.pretty_print_solution(s)
+    print()
+    if idx > 100:
+        break
--- tetraminoes_equal.py
+++ tetraminoes_equal.py
@@ -1,9 +1,11 @@
 import poly.parser as parser
 import poly.solver as solver
 
-figure = parser.load("shapes/figure.pol")
+figure = parser.load("shapes/figure_4x4.pol")
 
-for idx, s in enumerate(solver.auto_congruent_polyomino_split(figure, 5)):
+for idx, s in enumerate(solver.auto_congruent_polyomino_split(figure, 4)):
     parser.save_solution_to_svg(s, "output/" + str(idx) + "th_sol.svg")
     parser.pretty_print_solution(s)
     print()
+    if idx > 100:
+        break
