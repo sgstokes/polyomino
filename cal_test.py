61f930ac75017debcf8f3f96aac7f1771edfd8a1
sgstokes <s@sgstokes>
2022-04-01 12:08:53 -0400

    202204011208

--- cal_test.py
+++ cal_test.py
@@ -5,20 +5,13 @@ import poly.polyomino as pm
 
 figure = parser.load("shapes/cal_figure.pol")
 parts = ["L4", "L5", "O4", "P5", "T4", "U5", "X5", "Z4", "Z5"]
-pols = {}
+pols = set()
 
 print(figure, "\n")
 
 for p in parts:
     pol = parser.load(f"shapes/{p}.pol")
-    pols.update(pol)
-    print(pol, "\n")
-
-print(pols)
-
-pols = pm.generate(4)
-
-print(pols)
+    pols.add(pol)
 
 for idx, s in enumerate(solver.polomino_solve(figure, pols)):
     parser.save_solution_to_svg(s, "output/" + str(idx) + "th_sol.svg")
